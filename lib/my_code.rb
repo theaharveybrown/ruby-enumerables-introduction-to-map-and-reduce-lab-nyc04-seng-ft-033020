a0a74144cf45ca58c71d5cb8450cdc986d0c925d
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:14:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,5 +43,10 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   i = 0 
   total = starting_point
-  while i < source_array.length
+  while i < source_array.length do 
+    total += source_array[i]
+    i += 1 
+  end 
+end
+  
   
