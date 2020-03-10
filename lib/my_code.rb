e1e3ff553801e5937e351e80d60215831b110ab8
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:10:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+
+def map_to_nagativize(source_array) 
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i] * -1
+    i += 1 
+  end 
+  result
+end 
