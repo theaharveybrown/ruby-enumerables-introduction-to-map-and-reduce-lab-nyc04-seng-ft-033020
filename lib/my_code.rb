f77611e2f20da7ed1c6d148311918656a970b531
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:13:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,3 +30,12 @@ def map_to_double(source_array)
   result
 end
 
+def map_to_square(source_array)
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i] ** 2 
+    i += 1 
+  end 
+  result
+end 
