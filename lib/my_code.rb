84ec9c028ecbfd7e40840c8cecc3b3c30fa889c3
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:14:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,3 +39,9 @@ def map_to_square(source_array)
   end 
   result
 end 
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  total = starting_point
+  while i < source_array.length
+  
