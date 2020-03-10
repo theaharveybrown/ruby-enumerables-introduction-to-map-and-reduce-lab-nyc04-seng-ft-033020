3f431059715ab8fa65402c4c3b21ca8de342cf8c
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:12:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,13 @@ def map_to_negativize(source_array)
   end 
   result
 end 
+
+def map_to_no_change(source_array)
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i]
+    i += 1 
+  end 
+  result
+end 
