fa3d4e7d7b263282e0631f1c5b9cab18d103d576
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 13:17:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,76 @@
 # My Code here....
+
+def map_to_negativize(source_array) 
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i] * -1
+    i += 1 
+  end 
+  result
+end 
+
+def map_to_no_change(source_array)
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i]
+    i += 1 
+  end 
+  result
+end 
+
+def map_to_double(source_array)
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i] * 2
+    i += 1 
+  end 
+  result
+end
+
+def map_to_square(source_array)
+  i = 0
+  result = []
+  while i < source_array.length do 
+    result << source_array[i] ** 2 
+    i += 1 
+  end 
+  result
+end 
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  total = starting_point
+  while i < source_array.length do 
+    total += source_array[i]
+    i += 1 
+  end 
+  total 
+end
+  
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == false 
+      return false 
+    end
+    i += 1 
+  end 
+  return true 
+end 
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      return true 
+    end 
+    i += 1 
+  end 
+  return false 
+end 
+    
+    
+    
